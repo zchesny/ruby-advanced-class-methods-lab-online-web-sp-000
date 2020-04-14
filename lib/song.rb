18c2961ef25338ed738ed895ff21f4896b23a901
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 18:35:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,15 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize()
+  end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
   def self.all
     @@all
   end
